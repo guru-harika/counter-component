e5d69cd97036af8a328098df28fd9d9ac9d80f1e
guru-harika <114151375+guru-harika@users.noreply.github.com>
2024-09-18 10:50:27 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import Counter from "./components/Counter";
+function App() {
+  return (
+    <div>
+      <h1>My App Component</h1>
+      <Counter />
+      <Counter></Counter>
+    </div>
+  );
+}
+export default App;
